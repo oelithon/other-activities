af8813f4e217ed8554b546b13cb1fd556d5f005d
Elithon Silva <elithonsilva@gmail.com>
2021-08-18 15:51:20 -0300

    refac: teste exemplo-01 e adiciona comentario

--- testes-assincronos-jest/test/exemple.01.spec.js
+++ testes-assincronos-jest/test/exemple.01.spec.js
@@ -1,9 +1,15 @@
+// Exemplo onde se espera um erro:
+// Para que o Jest espere a função assíncrona ser finalizada, é necessário utilizar uma callback própria da biblioteca, chamada done , que precisa ser chamada após os testes assíncronos.
+// Nesse caso é preciso tembém envolver a asserção com um try/catch, dessa forma, caso ela falhe, chamamos a callback done dentro do bloco catch.
+
 test('Não deveria passar!', (done) => {
   setTimeout(() => {
-    expect(5).toBe(5);
-    console.log('Deveria falhar!');
-    done();
+    try {
+      expect(10).toBe(5);
+      console.log('Deveria falhar!');
+      done();
+    } catch (error) {
+      done(error);
+    }
   }, 500);
 });
-
-// Para que o Jest espere a função assíncrona ser finalizada, é necessário utilizar uma callback própria da biblioteca, chamada done , que precisa ser chamada após os testes assíncronos.
